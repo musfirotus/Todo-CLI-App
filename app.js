90678bea25f7c8c7765a7956dd16152a56473e85
Musfirotus <musfirotus@gmail.com>
2020-07-30 06:59:55 +0700

    hanya bisa listing...

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 'use strict'
 const { Sequelize } = require('sequelize');
+const koneksi = require('./config/db');
+const Todo = require('./models/Todo');
 const { program } = require('@caporal/core');
-const prompt = require('prompt-confirm');
-const koneksi = new Sequelize('todo', 'root', '200801', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-
-const Todo = koneksi.define('todo', {
-  item: Sequelize.TEXT,
-  status: Sequelize.STRING
-});
 
 program.version("1.0.0").description("ToDoList-CLI-APP");
 
@@ -25,15 +17,6 @@ program
             koneksi.close();
         })()
     })
-    .command('todo add', 'Add new todo item')
-    .argument('<item>', 'Item to add')
-    .action((args) => {
-        const addItem = List.build({ item: args.item });
-        addItem.save().then(() => {
-            console.log('new list saved')
-        }).finally(() => {
-            koneksi.close();
-        });
-    })
+    
 
 program.run();
