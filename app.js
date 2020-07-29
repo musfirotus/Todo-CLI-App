3fa6803fb4bc36390f6798964a5b9fb87a62f9c3
Musfirotus <musfirotus@gmail.com>
2020-07-29 22:58:23 +0700

    list & add

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
+'use strict'
 const { Sequelize } = require('sequelize');
-const { program } = require("@caporal/core");
-
-const sequelize = new Sequelize({
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    username: 'root',
-    password: '200801',
-    database: 'todo'
+const { program } = require('@caporal/core');
+const prompt = require('prompt-confirm');
+const koneksi = new Sequelize('todo', 'root', '200801', {
+  host: 'localhost',
+  dialect: 'mysql'
 });
 
-(async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-})();
+const Todo = koneksi.define('todo', {
+  item: Sequelize.TEXT,
+  status: Sequelize.STRING
+});
+
+program.version("1.0.0").description("ToDoList-CLI-APP");
+
+program
+    .command('todo list', 'Show all todo list in Database')
+    .action(() => {
+        (async () => {
+            let todolist = await Todo.findAll({ raw: true });
+            todolist.forEach(todo => {
+                console.log(`${todo.id}. ${todo.item}. (${todo.status})`)
+            })
+            koneksi.close();
+        })()
+    })
+    .command('todo add', 'Add new todo item')
+    .argument('<item>', 'Item to add')
+    .action((args) => {
+        const addItem = List.build({ item: args.item });
+        addItem.save().then(() => {
+            console.log('new list saved')
+        }).finally(() => {
+            koneksi.close();
+        });
+    })
+
+program.run();
